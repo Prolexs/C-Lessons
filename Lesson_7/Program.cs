e84d4719b825258535b3412412ff4a7e7bab7edf
Алексей Старченко <Prolex>
2022-12-15 17:34:29 +0300

    поиск макс значения

---
+++ Lesson_7/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5;
+int b = 7;
+int c = 8;
+int d = 14;
+int e = 3;
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
