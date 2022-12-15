37bedfd147657276b3dc8c4bfe3d9dade6a8b6aa
Алексей Старченко <Prolex>
2022-12-15 17:01:36 +0300

    сложение случайных чисел

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,6 @@
+﻿int NumberA = new Random() .Next(1,10);
+Console.WriteLine(NumberA);
+int NumberB = new Random() .Next(1,10);
+Console.WriteLine(NumberB);
+int Result = NumberA + NumberB;
+Console.WriteLine(Result);
