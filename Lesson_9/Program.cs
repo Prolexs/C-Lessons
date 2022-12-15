beb28374300570ed0d766ddc9b8ded9f4bb4460a
Алексей Старченко <Prolex>
2022-12-15 18:52:14 +0300

    задача с собакой

---
+++ Lesson_9/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+int distance = 10000;
+int FirsFriendSpeed = 5;
+int SecondFriendSpeed = 7;
+int DogSpeed = 15;
+int friend = 2;
+int time = 0;
+while(distance > 10)
+{
+    if(friend == 2)
+    {
+        time = distance / (SecondFriendSpeed + DogSpeed);
+        distance = distance - (FirsFriendSpeed + SecondFriendSpeed) * time;
+        count++;
+    }
+}
